2d0786ef872f49771f1d85162812c5fa61cf6149
blckclov3r <blckclov3r@gmail.com>
2019-09-15 08:33:12 -0800

    add bootstrap css&js

--- index.php
+++ index.php
@@ -6,10 +6,15 @@
         <title>Shopping Cart</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
         
-        
+        <h1>Helloworld</h1>
+       
+
+        <script src="js/jquery-3.4.1.min.js"></script>
+        <script src="js/bootstrap.js"></script>
     </body>
 </html>
