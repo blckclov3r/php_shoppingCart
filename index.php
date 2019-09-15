fafc686072ca10b26e316e77d1d7e456ca38a469
blckclov3r <blckclov3r@gmail.com>
2019-09-15 09:36:59 -0800

    add fonts folder for bootstrap glyphicon

--- index.php
+++ index.php
@@ -11,10 +11,29 @@
     </head>
     <body>
         
-        <h1>Helloworld</h1>
+        <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a href="#" class="navbar-brand">MyStore</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li><a href="#"><span class="glyphicon glyphicon-home"></span>Home</a></li>
+                    <li><a href="#"><span class="glyphicon glyphicon-modal-window"></span>Product</a></li>
+                    <li><input type="text" class="form-control" placeholder="Search..."id="search"></li>
+                    <li><input type="submit" class="btn btn-primary" value="Search" id="search_btn"></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span>Cart</a></li>
+                    <li><a href="#">Signin</a></li>
+                    <li><a href="#">Signup</a></li>
+                </ul>
+            </div>
+            
+        </div>
        
 
         <script src="js/jquery-3.4.1.min.js"></script>
+        <script src="js/main.js"></script>
         <script src="js/bootstrap.js"></script>
     </body>
 </html>
